51294a0a15a996d9b7fc9c6b84b1d9444d786ca3
Arno Pan <apan11@my.bcit.ca>
2025-09-17 15:52:13 -0700

    create dummy data for admin.php

---
+++ admin.php
@@ -0,0 +1,57 @@
+<?php
+$shops_data = [
+    [
+        'id' => 1,
+        'name' => 'The Daily Grind',
+        'address' => '123 Coffee St.',
+        'rating' => 4.5,
+        'hours_open' => '8 AM - 6 PM',
+        'food_availability' => 'Baked goods, sandwiches, salads',
+        'image_link' => 'uploads/images/the-daily-grind.jpg',
+        'neighbourhood' => 'Downtown'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Books & Brews',
+        'address' => '45 Reading Blvd.',
+        'rating' => 4.8,
+        'hours_open' => '9 AM - 9 PM',
+        'food_availability' => 'Coffee, tea, and light snacks',
+        'image_link' => 'uploads/images/books-and-brews.jpg',
+        'neighbourhood' => 'Arts District'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Urban Eatery',
+        'address' => '789 Main Ave.',
+        'rating' => 4.2,
+        'hours_open' => '11 AM - 10 PM',
+        'food_availability' => 'Burgers, fries, shakes',
+        'image_link' => 'uploads/images/urban-eatery.jpg',
+        'neighbourhood' => 'Midtown'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Garden Grill',
+        'address' => '10 Farm Rd.',
+        'rating' => 3.9,
+        'hours_open' => '12 PM - 8 PM',
+        'food_availability' => 'Vegan and vegetarian options',
+        'image_link' => 'uploads/images/garden-grill.jpg',
+        'neighbourhood' => 'The Meadows'
+    ],
+    [
+        'id' => 5,
+        'name' => 'Sweet Surrender',
+        'address' => '321 Dessert St.',
+        'rating' => 5.0,
+        'hours_open' => '10 AM - 7 PM',
+        'food_availability' => 'Cupcakes, macarons, pastries',
+        'image_link' => 'uploads/images/sweet-surrender.jpg',
+        'neighbourhood' => 'Old Town'
+    ]
+];
+
+
+
+?>
