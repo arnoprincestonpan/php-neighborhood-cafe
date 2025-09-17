c1af9e208c56bb0c266f1968ca226d719758dfa2
Arno Pan <apan11@my.bcit.ca>
2025-09-17 15:29:41 -0700

    Create table shops

--- db_setup.php
+++ db_setup.php
@@ -7,7 +7,20 @@ PURPOSE: To setup the database, assuming the server is not going to have data-fi
 
 try {
     $db = new PDO('sqlite:database.sqlite');
-    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $db->exec("CREATE TABLE IF NOT EXISTS shops(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        address TEXT NOT NULL,
+        rating REAL,
+        hours_open TEXT,
+        food_availability TEXT,
+        image_link TEXT,
+        neighbourhood TEXT
+        )
+    ");
+
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
